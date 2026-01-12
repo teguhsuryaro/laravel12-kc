6b87cbef258fd53f86761ce882615dd4d4fdd2a3
teguh surya <teguhsuryaro@gmail.com>
2026-01-12 22:39:18 +0700

    migration

---
+++ app/Models/Siswa.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Siswa extends Model
+{
+    //
+     protected $table = 'siswa';
+     protected $fillable = ['nama', 'tanggal_lahir', 'jurusan', 'nilai'];
+}
