ae2db4df0a494b412c70f1df9390921c485e9b12
teguh surya <teguhsuryaro@gmail.com>
2026-01-28 22:09:03 +0700

    add form login and register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,5 +2,103 @@
     <x-slot:title>
         Register Page
     </x-slot:title>
-    <h1 class="text-xl font-bold">Register Page</h1>
+    <section class="gradient-form h-full bg-neutral-200 dark:bg-neutral-700">
+        <div class="container h-full p-10">
+            <div
+            class="flex h-full flex-wrap items-center justify-center text-neutral-800 dark:text-neutral-200">
+            <div class="w-full">
+                <div
+                class="block rounded-lg bg-white shadow-lg dark:bg-neutral-800">
+                <div class="g-0 lg:flex lg:flex-wrap">
+                    <!-- Left column container-->
+                    <div class="px-4 md:px-0 lg:w-6/12">
+                    <div class="md:mx-6 md:p-12">
+                        <!--Logo-->
+                        <div class="flex flex-col text-center">
+                            <svg class="h-20" viewBox="0 0 28 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M0.41 10.3847C1.14777 7.4194 2.85643 4.7861 5.2639 2.90424C7.6714 1.02234 10.6393 0 13.695 0C16.7507 0 19.7186 1.02234 22.1261 2.90424C24.5336 4.7861 26.2422 7.4194 26.98 10.3847H25.78C23.7557 10.3549 21.7729 10.9599 20.11 12.1147C20.014 12.1842 19.9138 12.2477 19.81 12.3047H19.67C19.5662 12.2477 19.466 12.1842 19.37 12.1147C17.6924 10.9866 15.7166 10.3841 13.695 10.3841C11.6734 10.3841 9.6976 10.9866 8.02 12.1147C7.924 12.1842 7.8238 12.2477 7.72 12.3047H7.58C7.4762 12.2477 7.376 12.1842 7.28 12.1147C5.6171 10.9599 3.6343 10.3549 1.61 10.3847H0.41ZM23.62 16.6547C24.236 16.175 24.9995 15.924 25.78 15.9447H27.39V12.7347H25.78C24.4052 12.7181 23.0619 13.146 21.95 13.9547C21.3243 14.416 20.5674 14.6649 19.79 14.6649C19.0126 14.6649 18.2557 14.416 17.63 13.9547C16.4899 13.1611 15.1341 12.7356 13.745 12.7356C12.3559 12.7356 11.0001 13.1611 9.86 13.9547C9.2343 14.416 8.4774 14.6649 7.7 14.6649C6.9226 14.6649 6.1657 14.416 5.54 13.9547C4.4144 13.1356 3.0518 12.7072 1.66 12.7347H0V15.9447H1.61C2.39051 15.924 3.154 16.175 3.77 16.6547C4.908 17.4489 6.2623 17.8747 7.65 17.8747C9.0377 17.8747 10.392 17.4489 11.53 16.6547C12.1468 16.1765 12.9097 15.9257 13.69 15.9447C14.4708 15.9223 15.2348 16.1735 15.85 16.6547C16.9901 17.4484 18.3459 17.8738 19.735 17.8738C21.1241 17.8738 22.4799 17.4484 23.62 16.6547ZM23.62 22.3947C24.236 21.915 24.9995 21.664 25.78 21.6847H27.39V18.4747H25.78C24.4052 18.4581 23.0619 18.886 21.95 19.6947C21.3243 20.156 20.5674 20.4049 19.79 20.4049C19.0126 20.4049 18.2557 20.156 17.63 19.6947C16.4899 18.9011 15.1341 18.4757 13.745 18.4757C12.3559 18.4757 11.0001 18.9011 9.86 19.6947C9.2343 20.156 8.4774 20.4049 7.7 20.4049C6.9226 20.4049 6.1657 20.156 5.54 19.6947C4.4144 18.8757 3.0518 18.4472 1.66 18.4747H0V21.6847H1.61C2.39051 21.664 3.154 21.915 3.77 22.3947C4.908 23.1889 6.2623 23.6147 7.65 23.6147C9.0377 23.6147 10.392 23.1889 11.53 22.3947C12.1468 21.9165 12.9097 21.6657 13.69 21.6847C14.4708 21.6623 15.2348 21.9135 15.85 22.3947C16.9901 23.1884 18.3459 23.6138 19.735 23.6138C21.1241 23.6138 22.4799 23.1884 23.62 22.3947Z" fill="teal"></path>
+                            </svg>
+                        <h4 class="mb-12 mt-1 pb-1 text-xl font-semibold">
+                            We are The Lotus Team
+                        </h4>
+                        </div>
+
+                        <form action="{{ route('register') }}" method="POST">
+                            @csrf
+                            <p class="mb-4">Please register an account</p>
+                            <!--Username input-->
+                            <div class="relative mb-4 flex flex-col" data-twe-input-wrapper-init>
+                                <label for="name">Name</label>
+                                <input class="px-2 py-1 border-1 rounded" type="text" name="name" id="name">
+                            </div>
+                            
+                            <!--Email input-->
+                            <div class="relative mb-4 flex flex-col" data-twe-input-wrapper-init>
+                                <label for="email">Email</label>
+                                <input class="px-2 py-1 border-1 rounded" type="email" name="email" id="email">
+                            </div>
+
+                            <!--Password input-->
+                            <div class="relative mb-4 flex flex-col" data-twe-input-wrapper-init>
+                                <label for="password">Password</label>
+                                <input class="px-2 py-1 border-1 rounded" type="password" name="password" id="password">
+                            </div>
+
+                            <!--Password Confirmation input-->
+                            <div class="relative mb-4 flex flex-col" data-twe-input-wrapper-init>
+                                <label for="password_confirmation">Password Confirmation</label>
+                                <input class="px-2 py-1 border-1 rounded" type="password" name="password_confirmation" id="password_confirmation">
+                            </div>
+
+                            <!--Submit button-->
+                            <div class="mb-12 pb-1 pt-1 text-center">
+                                <button
+                                class="bg-teal-600 cursor-pointer hover:opacity-85 mb-3 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-dark-3 transition duration-150 ease-in-out hover:shadow-dark-2 focus:shadow-dark-2 focus:outline-none focus:ring-0 active:shadow-dark-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
+                                type="submit"
+                                data-twe-ripple-init
+                                data-twe-ripple-color="light">
+                                Sign Up
+                                </button>
+
+                                <!--Forgot password link-->
+                                <a href="#!">Remember me?</a>
+                            </div>
+
+                            <!--Register button-->
+                            <div class="flex items-center justify-between pb-6">
+                                <p class="mb-0 me-2">Have an account?</p>
+                                <a
+                                href="{{ route('auth.login') }}"
+                                class="inline-block rounded border-2 border-danger px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-danger transition duration-150 ease-in-out hover:border-danger-600 hover:bg-danger-50/50 hover:text-danger-600 focus:border-danger-600 focus:bg-danger-50/50 focus:text-danger-600 focus:outline-none focus:ring-0 active:border-danger-700 active:text-danger-700 dark:hover:bg-teal-950 dark:focus:bg-teal-950"
+                                data-twe-ripple-init
+                                data-twe-ripple-color="light">
+                                Login
+                                </a>
+                            </div>
+                        </form>
+                    </div>
+                    </div>
+
+                    <!-- Right column container with background and description-->
+                    <div
+                    class="bg-teal-600 flex items-center rounded-b-lg lg:w-6/12 lg:rounded-e-lg lg:rounded-bl-none">
+                    <div class="px-4 py-6 text-white md:mx-6 md:p-12">
+                        <h4 class="mb-6 text-xl font-semibold">
+                        We are more than just a company
+                        </h4>
+                        <p class="text-sm">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua. Ut enim ad minim veniam, quis
+                        nostrud exercitation ullamco laboris nisi ut aliquip ex
+                        ea commodo consequat.
+                        </p>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </section>
 </x-auth-layout>
